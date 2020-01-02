4b1ad808da42b03d7381dc2dcc7f885676357b15
euntas <tavstaus@hanmail.net>
2020-01-02 12:50:43 +0900

    Main 씬 추가, player 기본 이동

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:352f0f7a515fff96a3a2c2d0688a0ec0f3beef97d838ef79c6b9333c5911a4d4
+size 2295
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:67a512a339688254effaf7a888a8e4495b3456697599030c8392afb7e5f5749f
+size 1061
