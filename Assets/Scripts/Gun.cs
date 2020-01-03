1fab184196c72ef64d1ce4a7dd6d7cc201c70240
euntas <tavstaus@hanmail.net>
2020-01-03 10:08:59 +0900

    gitattribute 수정 후 커밋 테스트

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,3 +1,102 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4df5c44f703bd17f721f0a11f31f08a0bb9c3f7ed636b73ff442b862d1f21424
-size 3069
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{ // commit test
+    public enum State
+    {
+        Ready,
+        Empty,
+        Reloading
+    }
+
+    public State state { get; private set; } // 현재 총의 상태
+
+    public Transform fireTransform; // 탄알이 발사될 위치
+
+    public GameObject GunFlashObj; // TODO. 발사 때 잠깐 보여줄 빛 모형 오브젝트. 나중에 지울지도 모름.
+
+    private LineRenderer bulletLineRenderer; // 탄알 궤적을 그리기 위한 렌더러
+
+    private AudioSource gunAudioPlayer; // 총 소리 발사기
+    public AudioClip shotClip; // 발사 소리
+    public AudioClip reloadClip; // 재장전 소리
+
+    public float damage = 25; // 공격력
+    private float fireDistance = 50f; // 사정거리
+
+    public int ammoRemain = 100; // 남은 전체 탄알
+    public int magCapacity = 25; // 탄창 용량
+    public int magAmmo; // 현재 탄창에 남아 있는 탄알
+
+    public float timeBetFire = 0.12f; // 탄알 발사 간격
+    public float reloadTime = 1.8f; // 재장전 소요 시간
+    private float lastFireTime; // 총을 마지막으로 발사한 시점
+
+    private void Awake()
+    {
+        // 사용할 컴포넌트의 참조 가져오기
+        gunAudioPlayer = GetComponent<AudioSource>();
+        bulletLineRenderer = GetComponent<LineRenderer>();
+
+        // 사용할 점을 2개로 수정
+        bulletLineRenderer.positionCount = 2;
+        // 라인렌더러를 비활성화
+        bulletLineRenderer.enabled = false;
+    }
+
+    private void OnEnable()
+    {
+        // 현재 탄창을 가득 채우기
+        magAmmo = magCapacity;
+        // 총의 현재 상태를 총을 쏠 준비가 된 상태로 변경
+        state = State.Ready;
+        // 마지막으로 총을 쏜 시점을 초기화
+        lastFireTime = 0;
+    }
+
+    // 발사 시도
+    public void Fire()
+    {
+
+    }
+
+    // 실제 발사 처리
+    private void Shot()
+    {
+
+    }
+
+    // 발사 이펙트와 소리를 재생하고 탄알 궤적을 그림
+    private IEnumerator ShotEffect(Vector3 hitPosition)
+    {
+        // 라인렌더러를 활성화하여 탄알 궤적을 그림
+        bulletLineRenderer.enabled = true;
+
+        // 0.03초 동안 잠시 처리를 대기
+        yield return new WaitForSeconds(0.03f);
+
+        // 라인 렌더러를 비활성화하여 탄알 궤적을 지움
+        bulletLineRenderer.enabled = false;
+    }
+
+    // 재장전 시도
+    public bool Reload()
+    {
+        return false;
+    }
+
+    // 실제 재장전 처리를 진행
+    private IEnumerator ReloadRoutine()
+    {
+        // 현재 상태를 재장전 중 상태로 전환
+        state = State.Reloading;
+
+        // 재장전 소요 시간만큼 처리 쉬기
+        yield return new WaitForSeconds(reloadTime);
+
+        // 총의 현재 상태를 발사 준비된 상태로 변경
+        state = State.Ready;
+    }
+}
