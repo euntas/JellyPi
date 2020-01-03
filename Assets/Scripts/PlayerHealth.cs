d5adc4dfbb53c76a1ee260b34f7886567f8f0bd7
euntas <tavstaus@hanmail.net>
2020-01-03 17:12:14 +0900

    PlayerHealth, IItem 추가, 플레이어 체력 UI에 바인딩

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+// 플레이어 캐릭터의 생명체로서의 동작을 담당
+public class PlayerHealth : LivingEntity
+{
+    public Scrollbar healthScrollbar; // 체력을 표시할 스크롤바
+
+    public AudioClip deathClip; // 사망 소리
+    public AudioClip hitClip; // 피격 소리
+    public AudioClip itemPickupClip; // 아이템 습득 소리
+
+    private AudioSource playerAudioPlayer; // 플레이어 소리 재생기
+    private Animator playerAnimator; // 플레이어의 애니메이터
+
+    private PlayerMovement playerMovement; // 플레이어 움직임 컴포넌트
+    private PlayerShooter playerShooter; // 플레이어 슈터 컴포넌트
+
+    private void Awake()
+    {
+        // 사용할 컴포넌트 가져오기
+        playerAnimator = GetComponent<Animator>();
+        playerAudioPlayer = GetComponent<AudioSource>();
+
+        playerMovement = GetComponent<PlayerMovement>();
+        playerShooter = GetComponent<PlayerShooter>();
+    }
+
+    protected override void OnEnable()
+    {
+        // LivingEntity의 OnEnable() 실행(상태 초기화)
+        base.OnEnable();
+
+        // 체력 스크롤바 
+        healthScrollbar.gameObject.SetActive(true);
+        healthScrollbar.size = health / startingHealth;
+
+        // 플레이어 조작을 받는 컴포넌트 활성화
+        playerMovement.enabled = true;
+        playerShooter.enabled = true;
+    }
+
+    // 체력 회복
+    public override void RestoreHealth(float newHealth)
+    {
+        base.RestoreHealth(newHealth);
+
+        // 갱신된 체력으로 체력 스크롤바 갱신
+        healthScrollbar.size = newHealth / startingHealth;
+    }
+
+    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal)
+    {
+        if(!dead)
+        {
+            // 사망하지 않은 경우에만 효과음 재생
+            playerAudioPlayer.PlayOneShot(hitClip);
+        }
+
+        base.OnDamage(damage, hitPoint, hitNormal);
+        // 갱신된 체력으로 체력 스크롤바 갱신
+        healthScrollbar.size = health / startingHealth;
+    }
+
+    public override void Die()
+    {
+        base.Die();
+
+        // 사망음 재생
+        playerAudioPlayer.PlayOneShot(deathClip);
+        // 애니메이터의 Die 설정
+        playerAnimator.SetBool("Death_b", true);
+        playerAnimator.SetInteger("DeathType_int", 1);
+
+        // 플레이어 조작을 받는 컴포넌트 비활성화
+        playerMovement.enabled = false;
+        playerShooter.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 아이템과 충돌한 경우 해당 아이템을 사용하는 처리
+        if (!dead)
+        {
+            // 충돌상 상대방으로부터 IItem 컴포넌트 가져오기 시도
+            IItem item = other.GetComponent<IItem>();
+
+            // 충돌한 상대방으로부터 IItem 컴포넌트를 가져오는 데 성공했다면
+            if (item != null)
+            {
+                // Use 메서드를 실행하여 아이템 사용
+                item.Use(gameObject);
+                // 아이템 습득 소리 재생
+                playerAudioPlayer.PlayOneShot(itemPickupClip);
+            }
+        }
+    }
+}
