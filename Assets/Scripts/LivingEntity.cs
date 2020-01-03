1c4fdace72a8e17e4b758b05c93c937eb9bc0657
euntas <tavstaus@hanmail.net>
2020-01-03 10:12:47 +0900

    gitattribute 수정 후 코드 재업로드

--- Assets/Scripts/LivingEntity.cs
+++ Assets/Scripts/LivingEntity.cs
@@ -1,3 +1,50 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7724ddcdb7c281d55adb98d9bd019a8c8df20113d538395e37cf33b84dfd3bbe
-size 1261
+﻿using System;
+using UnityEngine;
+
+// 생명체로서 동작할 게임 오브젝트들을 위한 뼈대를 제공
+// 체력, 데미지 받아들이기, 사망 기능, 사망 이벤트를 제공
+public class LivingEntity : MonoBehaviour
+{
+    public float startingHealth = 100f; // 시작 체력
+    public float health { get; protected set; } // 현재 체력
+    public bool dead { get; protected set; } // 사망 상태
+    public event Action onDeath; // 사망시 발동할 이벤트
+
+    // 생명체가 활성화될때 상태를 리셋
+    protected virtual void OnEnable()
+    {
+        dead = false;
+        health = startingHealth;
+    }
+
+    public virtual void OnDamage(float damage, Vector3 hitPoint, Vector3 hitNormal)
+    {
+        health -= damage;
+
+        if (health <= 0 && !dead)
+        {
+            Die();
+        }
+    }
+
+    public virtual void RestoreHealth(float newHealth)
+    {
+        if (dead)
+        {
+            // 이미 사망한 경우 체력을 회복할 수 없음
+            return;
+        }
+
+        health += newHealth;
+    }
+
+    public virtual void Die()
+    {
+        if (onDeath != null)
+        {
+            onDeath();
+        }
+
+        dead = true;
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,3 +1,45 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2005abff27037ebcbd31ec475df6ba9f84e8dc9a30ed311e62fc65c01771f814
-size 1071
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                m_instance = FindObjectOfType<UIManager>();
+            }
+
+            return m_instance;
+        }
+    }
+
+    public GameObject gameoverUI; // 게임 오버시 활성화할 UI
+    public GameObject settingUI; // 게임 세팅 메뉴 UI
+    public static UIManager m_instance; // 싱글톤이 할당될 변수
+
+    // 게임 오버 UI 활성화
+    public void SetActiveGameoverUI(bool active)
+    {
+        gameoverUI.SetActive(active);
+    }
+
+    // 게임 종료시 UI
+    public void SetActiveSettingUI(bool active)
+    {
+        settingUI.SetActive(active);
+    }
+
+    // 게임 재시작
+    public void GameRestart()
+    {
+        SetActiveSettingUI(false);
+
+        SceneManager.LoadScene("Main");
+    }
+}
