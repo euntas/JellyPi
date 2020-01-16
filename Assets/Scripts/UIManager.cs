8049637ad41a5238cbfaabc520cc368c48619e1f
euntas <tavstaus@hanmail.net>
2020-01-16 17:31:28 +0900

    stage clear 기능 추가

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -24,6 +24,7 @@ public class UIManager : MonoBehaviour
     public GameObject gameoverUI; // 게임 오버시 활성화할 UI
     public GameObject settingUI; // 게임 세팅 메뉴 UI
     public GameObject shopUI; // 게임 상점 UI
+    public GameObject stageClearUI; // 게임 스테이지 클리어 UI
 
     public Text ammoText; // 탄약 표시용 텍스트
     public Text playerHPText; // 플레이어 hp 표시용 텍스트
@@ -31,6 +32,7 @@ public class UIManager : MonoBehaviour
     public Text scoreText; // 점수 표시용 텍스트
     public Text stageNameText; // 스테이지 이름 텍스트
     public Text goldText; // 골드 텍스트
+    public Text stageClearText; // 스테이지 클리어 텍스트
 
     // 게임 오버 UI 활성화
     public void SetActiveGameoverUI(bool active)
@@ -56,6 +58,14 @@ public class UIManager : MonoBehaviour
         shopUI.SetActive(active);
     }
 
+    // 게임 스테이지 클리어 UI
+    public void SetActiveStageClearUI(bool active)
+    {
+        GameManager.instance.PauseGame(active);
+
+        stageClearUI.SetActive(active);
+    }
+
     // 게임 재시작
     public void GameRestart()
     {
@@ -73,7 +83,7 @@ public class UIManager : MonoBehaviour
     // 플레이어 HP 텍스트 갱신
     public void UpdatePlayerHPText(int currentHP, int maxHP)
     {
-        int displayHP = (currentHP < 0) ? 0 : currentHP; 
+        int displayHP = (currentHP < 0) ? 0 : currentHP;
         playerHPText.text = "HP " + displayHP + "/" + maxHP;
     }
 
@@ -99,5 +109,11 @@ public class UIManager : MonoBehaviour
     public void UpdateGoldText(int gold)
     {
         goldText.text = gold.ToString();
+    }
+
+    // 스테이지 클리어 텍스트 갱신
+    public void UpdateStageClearText(string _stageName, int _gold)
+    {
+        stageClearText.text = "Stage [ " + _stageName + " ] Clear!\nGold: " + _gold.ToString();
     }
 }
