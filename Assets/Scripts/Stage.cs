4ea5b01f3cd354c9a4493fd6ebf259bf88faf177
euntas <tavstaus@hanmail.net>
2020-01-16 15:56:13 +0900

    Stage 클래스 추가, gold 변수 추가, UI 수정

---
+++ Assets/Scripts/Stage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage : MonoBehaviour
+{
+    public int stageId { get; private set; } // 스테이지 고유 번호
+    public string stageName; // 스테이지 이름
+    public bool isStageClear { get; private set; } // 스테이지 클리어 상태
+    public int totalWaveNum;  // 총 wave 수
+    public Vector3 playerPoint; // 플레이어 초기 위치
+    public int[] spawnPointNums; // 적 AI를 소환할 위치
+
+    // Stage 초기화
+    public void InitStage(int _stageId)
+    {
+        isStageClear = false;
+
+        // TODO. 나중에 스테이지 ID 별로 정보를 불러오는 식으로 바꿔야 함
+        SetStage(_stageId, "First Stage!!", 2, new Vector3(0, 0, 0), new int[] { 1, 2, 3 });
+    }
+
+    // Stage 정보 세팅
+    public void SetStage(int _stageId, string _stageName, int _totalWaveNum, Vector3 _playerPoint, int[] _spawnPointNums)
+    {
+        stageId = _stageId;
+        stageName = _stageName;
+        totalWaveNum = _totalWaveNum;
+        playerPoint = _playerPoint;
+        spawnPointNums = _spawnPointNums;
+    }
+}
